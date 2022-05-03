5363dec0f61977c758428cf764c2490e6dddab65
andreportol <andreportol@gmail.com>
2022-05-03 14:14:59 -0400

    Acrescentado um módulo para calcular a data com precisão.

--- aula_GIT.py
+++ aula_GIT.py
@@ -1,4 +1,13 @@
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+
 nome = input('Digite seu primeiro nome:')
-ano_nascimento = int(input('Data de nascimento:'))
-idade = 2022 - ano_nascimento
-print(f'{nome} tem {idade} anos.')
+ano_nascimento_String = input('Data de nascimento:')
+ano_nascimento_date = datetime.strptime(ano_nascimento_String,'%d/%m/%Y').date()
+# ano_nascimento_formatado = ano_nascimento_date.strftime('%d/%m/%Y')
+
+data_hoje = date.today()
+# data_hoje_formatada = data_hoje.strftime('%d/%m/%Y')
+
+idade = relativedelta(data_hoje, ano_nascimento_date)
+print(f'{nome} tem {idade.years} anos.')
