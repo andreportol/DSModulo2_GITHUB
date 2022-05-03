0ee99b45417c1a382cb33fb324d041578feaa371
andreportol <andreportol@gmail.com>
2022-05-03 12:55:07 -0400

    transformando a variável ano_nascimento para int

--- aula_GIT.py
+++ aula_GIT.py
@@ -1,4 +1,4 @@
 nome = input('Digite seu primeiro nome:')
-ano_nascimento = input('Data de nascimento:')
+ano_nascimento = int(input('Data de nascimento:'))
 idade = 2022 - ano_nascimento
 print(f'{nome} tem {idade} anos.')
